be1a64136e245031fb1592d9bade4f231f45587c
Nickolas Tenney <ntenney@spillman.com>
2017-09-18 14:43:54 -0600

    updates

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -4,7 +4,7 @@ import '../App.css';
 export default function Body() {
     return (
         <div className="body-bg">
-            <img className="body-bg-size" src="http://www.menghishoes.com/wp-content/uploads/banner-11.jpg" alt="shoe" />
+            
         </div>
     );
    
