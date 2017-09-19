977cff18fe88609afb27c7e0306b27bfc99b20c0
Nickolas Tenney <ntenney@spillman.com>
2017-09-19 13:43:02 -0600

    sizing

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -3,8 +3,13 @@ import '../App.css';
 
 export default function Body() {
     return (
+        <div>
         <div className="body-bg">
-            
+            <img src="http://www.menghishoes.com/wp-content/uploads/menghi_logo-300x88.png" alt="logo"/>
+        </div>
+            <div className="break">
+            <p><i>Made in Italy</i></p>
+            </div>
         </div>
     );
    
